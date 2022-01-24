7600117999659dbb216acb08a2d43d0b4c46b4b8
Paras-Roy <parasandroy@gmail.com>
2022-01-25 04:42:35 +0530

    Final changes

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-// window.addEventListener('scroll', function(){
-//     let nav = document.querySelector("nav");
-//     nav.classList.toggle('dark', window.scrollY>0)
-// });
+const sectionAll = document.querySelectorAll('section[id]');
+window.addEventListener('scroll', ()=>{
+const scrollY = window.pageYOffset;
+    sectionAll.forEach((current)=>{
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 200;
+        const sectionId = current.getAttribute('id');
+
+        
+        if (scrollY > sectionTop && scrollY < sectionTop + sectionHeight){
+            document.querySelector('nav a[href*="'+ sectionId +'"]').classList.add('navActive');
+            // console.log(sectionId);
+        }
+
+
+        else {
+            document.querySelector('nav a[href*="'+ sectionId +'"]').classList.remove('navActive');
+            
+        }
+});
+});
